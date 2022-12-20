897e88dabc99283e891d6f8dbad25ccca7a787b9
Lefu Ntho <lefu.ntho@payfast.co.za>
2022-12-20 12:39:51 +0200

    first commit for fix

--- lib/Auth.php
+++ lib/Auth.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace PayFast;
 
 
@@ -52,36 +51,40 @@ class Auth
      * @return string
      * @throws InvalidRequestException
      */
-    public static function generateSignature($data, $passPhrase = null): string {
-        $sortAttributes = [
-            'merchant_id', 'merchant_key', 'return_url', 'cancel_url', 'notify_url',
-            'name_first', 'name_last', 'email_address', 'cell_number',
-            'm_payment_id', 'amount', 'item_name', 'item_description',
-            'custom_int1', 'custom_int2', 'custom_int3', 'custom_int4', 'custom_int5',
-            'custom_str1', 'custom_str2', 'custom_str3', 'custom_str4', 'custom_str5',
-            'email_confirmation', 'confirmation_address',
-            'payment_method',
-            'subscription_type', 'billing_date', 'recurring_amount', 'frequency', 'cycles'
-        ];
+    public static function generateSignature($data, $passPhrase = null): string
+    {
+        $fields = ['merchant_id', 'merchant_key', 'return_url', 'cancel_url', 'notify_url', 'notify_method',
+            'name_first', 'name_last', 'email_address', 'cell_number', 'm_payment_id', 'amount', 'item_name',
+            'item_description', 'custom_int1', 'custom_int2', 'custom_int3', 'custom_int4', 'custom_int5',
+            'custom_str1', 'custom_str2', 'custom_str3', 'custom_str4', 'custom_str5', 'email_confirmation',
+            'confirmation_address', 'currency', 'payment_method', 'subscription_type', 'passphrase',
+            'billing_date', 'recurring_amount', 'frequency', 'cycles', 'subscription_notify_email',
+            'subscription_notify_webhook', 'subscription_notify_buyer'];
+
+        $sortAttributes = array_filter($data, function ($key) use ($fields) {
+            return in_array($key, $fields);
+        }, ARRAY_FILTER_USE_KEY);
+
+        if($passPhrase !== null && $passPhrase !== '') {
+            $sortAttributes['passphrase'] = urlencode(trim($passPhrase));
+        }
 
         // Some functionality requires the passphrase to be set
-        if(isset($data['subscription_type']) && ($passPhrase === null || $passPhrase === '')) {
+        if (isset($data['subscription_type']) && ($passPhrase === null || $passPhrase === '')) {
             throw new InvalidRequestException('Subscriptions require a passphrase to be set', 400);
         }
 
         // Create parameter string
         $pfOutput = '';
-        foreach( $sortAttributes as $attribute ) {
-            if(array_key_exists($attribute, $data) && $data[$attribute] !== '') {
-                $pfOutput .= $attribute .'='. urlencode( trim( $data[$attribute] ) ) .'&';
+        foreach($sortAttributes as $attribute => $value) {
+            if(!empty($value)) {
+                $pfOutput .= $attribute .'='. urlencode(trim($value)) .'&';
             }
         }
         // Remove last ampersand
-        $getString = substr( $pfOutput, 0, -1 );
-        if( $passPhrase !== null && $passPhrase !== '' ) {
-            $getString .= '&passphrase='. urlencode( trim( $passPhrase ) );
-        }
-        return md5( $getString );
+        $getString = substr($pfOutput, 0, -1);
+
+        return md5($getString);
     }
 
 }
